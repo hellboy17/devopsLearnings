eb90c7a1ceaae0d001a9c112dc0b06f1a78dc110
SOUVIK <hellboyone68@gmail.com>
2025-02-10 19:25:46 +0530

    third commit

--- src/main/java/mypkg/MyCalc.java
+++ src/main/java/mypkg/MyCalc.java
@@ -9,11 +9,15 @@ public class MyCalc {
 		
 		return a-b;
 	}
-	
+	public int mul(int a, int b) {
+		
+		return a*b;
+	}
 	public static void main(String[] args) {
 		MyCalc calc = new MyCalc();
 		System.out.println("Sum is: "+calc.sum(20, 10));
 		System.out.println("Sum is: "+calc.sub(20, 10));
+		System.out.println("Sum is: "+calc.mul(20, 10));
 	}
 
 }
