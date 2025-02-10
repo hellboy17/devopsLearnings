e7a68c72d07766281e41cd528fab55d6c299fc48
SOUVIK <hellboyone68@gmail.com>
2025-02-10 19:51:02 +0530

    4th commit

--- src/main/java/mypkg/MyCalc.java
+++ src/main/java/mypkg/MyCalc.java
@@ -13,11 +13,16 @@ public class MyCalc {
 		
 		return a*b;
 	}
+	public int div(int a, int b) {
+		
+		return a*b;
+	}
 	public static void main(String[] args) {
 		MyCalc calc = new MyCalc();
 		System.out.println("Sum is: "+calc.sum(20, 10));
 		System.out.println("Sum is: "+calc.sub(20, 10));
 		System.out.println("Sum is: "+calc.mul(20, 10));
+		System.out.println("Sum is: "+calc.div(20, 10));
 	}
 
 }
